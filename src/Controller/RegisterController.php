ad1f6255672a882f5029dad2b71a4d463f6992e7
Antoine <antoinepascualmail@gmail.com>
2021-02-09 14:21:26 +0100

    Ajout page accueil/connexion/inscription

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class RegisterController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/account/register', name: 'register')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
         ]);
     }
 }
